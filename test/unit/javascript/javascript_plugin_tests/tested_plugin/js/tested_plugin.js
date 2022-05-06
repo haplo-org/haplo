da039b77d39bd429dd3ffb368b24173bee298b11
andreivictorserban <93380791+andreivictorserban@users.noreply.github.com>
2022-05-06 10:19:16 +0100

    exposing status code for tests (#19)
    * exposing status code for tests
    
    * added Avalara copyright statement
    
    * added asserts for the statusCode and explicitly making statusCode = 200 in request handlers
    
    * defaulting to 200 if not response.statusCode supplied

--- test/unit/javascript/javascript_plugin_tests/tested_plugin/js/tested_plugin.js
+++ test/unit/javascript/javascript_plugin_tests/tested_plugin/js/tested_plugin.js
@@ -1,5 +1,6 @@
 /* Haplo Platform                                     http://haplo.org
  * (c) Haplo Services Ltd 2006 - 2016    http://www.haplo-services.com
+ * (c) Avalara, Inc 2021
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.         */
@@ -9,6 +10,7 @@ P.respond("GET", "/do/tested_plugin/handler1", [
     {pathElement:0, as:"int"}
 ], function(E, number) {
     E.response.kind = 'text';
+    E.response.statusCode = 200;
     E.response.body = "i="+number+" u="+O.currentUser.id;
 });
 
@@ -31,6 +33,7 @@ P.respond("POST", "/do/tested_plugin/posting2", [
 ], function(E) {
     tested_plugin.requestAsSeenByPlugin = E.request;
     E.response.kind = 'json';
+    E.response.statusCode = 200;
     E.response.body = E.request.body;
 });
 
--- test/unit/javascript/javascript_plugin_tests/tested_plugin/test/tested_plugin_request1.js
+++ test/unit/javascript/javascript_plugin_tests/tested_plugin/test/tested_plugin_request1.js
@@ -1,5 +1,6 @@
 /* Haplo Platform                                     http://haplo.org
  * (c) Haplo Services Ltd 2006 - 2016    http://www.haplo-services.com
+ * (c) Avalara, Inc 2021
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.         */
@@ -10,13 +11,16 @@ t.test(function() {
     t.login("user1@example.com");
 
     t.get("/do/tested_plugin/handler1/2445");
+    t.assertEqual(t.last.statusCode, 200);
     t.assertEqual(t.last.body, "i=2445 u=41");
     t.assertEqual(t.last.view, undefined);
 
     t.get("/do/tested_plugin/handler1/2446");
+    t.assertEqual(t.last.statusCode, 200);
     t.assertEqual(t.last.body, "i=2446 u=41");
 
     t.get("/do/tested_plugin/handler2", {z:"Ping"});
+    t.assertEqual(t.last.statusCode, 200);
     t.assertEqual(t.last.method, "GET");
     t.assertEqual(t.last.body, "HANDLER2: Ping");
     t.assertEqual(t.last.templateName, "handler2");
@@ -34,11 +38,13 @@ t.test(function() {
     t.assertEqual(requestAsSeenByPlugin.remote.protocol, "IPv4");
     t.assertEqual(requestAsSeenByPlugin.remote.address, "10.1.2.3");
     t.assertEqual(postLast.templateName, "posting_template");
+    t.assertEqual(postLast.statusCode, 200);
     t.assertEqual(postLast.body, "POSTED");
     t.assertEqual(postLast.view.pageTitle, "Post");
 
     var postLast2 = t.post("/do/tested_plugin/posting2", {}, {body:{h1:"value1", h2:"value2"}, kind:"json"});
     var requestAsSeenByPlugin = tested_plugin.requestAsSeenByPlugin;
+    t.assertEqual(postLast2.statusCode, 200);
     t.assertObject(JSON.parse(requestAsSeenByPlugin.body), {h1:"value1", h2:"value2"});
     t.assertObject(JSON.parse(requestAsSeenByPlugin.body), {h2:"value2", h1:"value1"});
     t.assertEqual(requestAsSeenByPlugin.kind, "json");
